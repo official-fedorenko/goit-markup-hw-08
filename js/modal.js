e8b793e88d029e59861efd62193a5349cb5f1d3a
Roman <official.fedorenko@gmail.com>
2020-10-12 22:57:51 +0300

    2

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,18 @@
       refs.modal.classList.toggle('is-hidden');
     }
   })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[menu-modal-open]'),
+      closeModalBtn: document.querySelector('[menu-modal-close]'),
+      modal: document.querySelector('[menu-modal]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-open');
+    }
+  })();
